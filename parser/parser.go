c68d8c6457a8469d59ade4ff6f9d9e605ea298a1
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2023-09-19 22:10:27 -0600

    Up to page 57.

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/mdwhatcott/waiig/ast"
 	"github.com/mdwhatcott/waiig/lexer"
 	"github.com/mdwhatcott/waiig/token"
@@ -9,6 +11,7 @@ import (
 type Parser struct {
 	l *lexer.Lexer
 
+	errors    []string
 	curToken  token.Token
 	peekToken token.Token
 }
@@ -23,6 +26,10 @@ func New(l *lexer.Lexer) *Parser {
 	return p
 }
 
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
@@ -31,9 +38,9 @@ func (p *Parser) nextToken() {
 func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
 	for p.curToken.Type != token.EOF {
-		stmt := p.parseStatement()
-		if stmt != nil {
-			program.Statements = append(program.Statements, stmt)
+		statement := p.parseStatement()
+		if statement != nil {
+			program.Statements = append(program.Statements, statement)
 		}
 		p.nextToken()
 	}
@@ -74,6 +81,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	}
+	p.peekError(t)
 	return false
 }
 
@@ -82,4 +90,8 @@ func (p *Parser) curTokenIs(t token.TokenType) bool {
 }
 func (p *Parser) peekTokenIs(t token.TokenType) bool {
 	return p.peekToken.Type == t
+}
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,6 +9,23 @@ import (
 	"github.com/mdwhatcott/waiig/token"
 )
 
+func TestErrors(t *testing.T) {
+	input := `
+let x 5;
+let = 10;
+let 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	should.So(t, program, should.Equal, &ast.Program{})
+	should.So(t, p.Errors(), should.Equal, []string{
+		"expected next token to be =, got INT instead",
+		"expected next token to be IDENT, got = instead",
+		"expected next token to be IDENT, got INT instead",
+	})
+}
+
 func TestLetStatement(t *testing.T) {
 	input := `
 let x = 5;
@@ -17,7 +34,9 @@ let foobar = 838383;
 `
 	l := lexer.New(input)
 	p := New(l)
-	should.So(t, p.ParseProgram(), should.Equal, &ast.Program{
+	program := p.ParseProgram()
+	should.So(t, p.Errors(), should.BeNil)
+	should.So(t, program, should.Equal, &ast.Program{
 		Statements: []ast.Statement{
 			&ast.LetStatement{
 				Token: token.New(token.LET, "let"),
